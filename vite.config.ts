5f7eeeb445158d61f82767cb8f868ce79ee7b4a9
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-09-03 22:14:09 +0300

    hv

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     sourcemap: true,
   },
-  base: "/02-react-cafe/", 
 });
